b75116dc452acd248c7e6cd4c9f40ce0c02fe9d5
Asadullah <asad.au.ullah@gmail.com>
2026-06-28 12:33:43 +0500

    Added CORS policy

--- PawCare.Server/PawCare.Server/Program.cs
+++ PawCare.Server/PawCare.Server/Program.cs
@@ -2,6 +2,75 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string PawCareCorsPolicy = "_PawCareCorsPolicy";
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>()
+                     ?? Array.Empty<string>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: PawCareCorsPolicy,
+        policy =>
+        {
+            policy.WithOrigins(allowedOrigins)
+                  .AllowAnyHeader()
+                  .AllowCredentials();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                policy.AllowAnyMethod();
+            }
+            else
+            {
+                policy.WithMethods("GET", "POST", "PUT", "DELETE");
+            }
+        });
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: PawCareCorsPolicy,
+        policy =>
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                policy.WithOrigins("http://localhost:5173")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            }
+            else
+            {
+                policy.WithOrigins("https://app.myproduct.com")
+                      .AllowAnyHeader()
+                      .WithMethods("GET", "POST", "PUT", "DELETE")
+                      .AllowCredentials();
+            }
+        });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: PawCareCorsPolicy,
+        policy =>
+        {
+            if (builder.Environment.IsDevelopment())
+            {                
+                policy.WithOrigins("http://localhost:5173")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            }
+            else
+            {                
+                policy.WithOrigins("https://app.myproduct.com")
+                      .AllowAnyHeader()
+                      .WithMethods("GET", "POST", "PUT", "DELETE")
+                      .AllowCredentials();
+            }
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,6 +86,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/healthcheck", () => "Healthy API");
+app.UseCors(PawCareCorsPolicy);
+
+app.MapGet("/healthcheck", () => new { status = "Healthy API" });
 
 app.Run();
